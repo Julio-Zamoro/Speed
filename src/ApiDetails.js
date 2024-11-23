a6a635619695db820809369b43f7e2595ba192e5
Julio <juliocesarsian@gmail.com>
2024-11-23 14:15:49 -0300

    Acrescentado a opção de alterar tipo de gráfico

--- src/ApiDetails.js
+++ src/ApiDetails.js
@@ -1,12 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Typography, CssBaseline, CircularProgress, MenuItem, Select } from '@mui/material';
-import { Line } from 'react-chartjs-2';
-import axios from 'axios';
+import {
+    Container,
+    Typography,
+    CssBaseline,
+    CircularProgress,
+    MenuItem,
+    Select,
+    Button,
+} from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Chart as ChartJS, LineElement, PointElement, CategoryScale, LinearScale, Title, Tooltip, Legend } from 'chart.js';
+import { Line, Bar } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    LineElement,
+    BarElement,
+    PointElement,
+    CategoryScale,
+    LinearScale,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import axios from 'axios';
 import './index.css';
 
-ChartJS.register(LineElement, PointElement, CategoryScale, LinearScale, Title, Tooltip, Legend);
+// ChartJS configuration
+ChartJS.register(LineElement, BarElement, PointElement, CategoryScale, LinearScale, Title, Tooltip, Legend);
 
 const darkTheme = createTheme({
     palette: {
@@ -36,75 +55,95 @@ function ApiDetails({ apiUrl, apiName }) {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [timeFilter, setTimeFilter] = useState('last24h');
+    const [selectedDatabase, setSelectedDatabase] = useState('Database1');
+    const [chartType, setChartType] = useState('line');
+
+    const toggleChartType = () => {
+        setChartType((prevType) => (prevType === 'line' ? 'bar' : 'line'));
+    };
 
     useEffect(() => {
         const fetchLogs = async () => {
             setLoading(true);
             setError(null);
             try {
-                const response = await axios.get(`http://localhost:3001/api/logs/rota/${encodeURIComponent(apiUrl)}`);
-                console.log('Dados recebidos:', response.data); // Log para verificar os dados recebidos
+                const response = await axios.get(`http://localhost:3001/api/logs/rota/${encodeURIComponent(apiUrl)}`, {
+                    params: { database: selectedDatabase },
+                });
                 setLogs(response.data);
-            } catch (error) {
-                setError(error.message);
+            } catch (err) {
+                setError(err.message);
             } finally {
                 setLoading(false);
             }
         };
 
         fetchLogs();
-    }, [apiUrl]);
+    }, [apiUrl, selectedDatabase]);
 
     const filterLogs = (logs) => {
         const now = new Date();
-        switch (timeFilter) {
-            case 'last12h':
-                return logs.filter(log => new Date(log.data_requisicao) > new Date(now - 12 * 60 * 60 * 1000));
-            case 'last24h':
-                return logs.filter(log => new Date(log.data_requisicao) > new Date(now - 24 * 60 * 60 * 1000));
-            case 'last7d':
-                return logs.filter(log => new Date(log.data_requisicao) > new Date(now - 7 * 24 * 60 * 60 * 1000));
-            case 'last30d':
-                return logs.filter(log => new Date(log.data_requisicao) > new Date(now - 30 * 24 * 60 * 60 * 1000));
-            default:
-                return logs;
-        }
+        const timeDurations = {
+            last12h: 12 * 60 * 60 * 1000,
+            last24h: 24 * 60 * 60 * 1000,
+            last7d: 7 * 24 * 60 * 60 * 1000,
+            last30d: 30 * 24 * 60 * 60 * 1000,
+        };
+        const filterTime = timeDurations[timeFilter];
+
+        return logs.filter((log) => new Date(log.data_requisicao) > new Date(now - filterTime));
     };
 
     const filteredLogs = filterLogs(logs);
 
     const data = {
-        labels: filteredLogs.slice().reverse().map((log) => {
+        labels: filteredLogs.map((log) => {
             const date = new Date(log.data_requisicao);
-            return `${String(date.getDate()).padStart(2, '0')}-${String(date.getMonth() + 1).padStart(2, '0')}-${date.getFullYear()} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
+            return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
         }),
         datasets: [
             {
                 label: 'Tempo de Resposta (ms)',
-                data: filteredLogs.slice().reverse().map((log, index) => {
-                    if (!log.tempo_requisicao) {
-                        console.error(`Tempo de requisição ausente no log de índice ${index}:`, log);
-                        return null; // Retorna `null` se o tempo de requisição estiver ausente
-                    }
-                    const tempoMs = typeof log.tempo_requisicao === 'object'
-                        ? (log.tempo_requisicao.minutes * 60 + log.tempo_requisicao.seconds) * 1000
-                        : (log.tempo_requisicao > 1000 ? log.tempo_requisicao : log.tempo_requisicao * 1000); // Converte para ms se estiver em segundos
-                    console.log(`Log ${index} - Tempo de Requisição Original: ${log.tempo_requisicao}, Convertido: ${tempoMs} ms`); // Log para verificar valores
+                data: filteredLogs.map((log) => {
+                    if (!log.tempo_requisicao) return null;
+                    const tempoMs =
+                        typeof log.tempo_requisicao === 'object'
+                            ? (log.tempo_requisicao.minutes * 60 + log.tempo_requisicao.seconds) * 1000
+                            : log.tempo_requisicao * 1000;
                     return isNaN(tempoMs) || tempoMs <= 0 ? null : tempoMs;
                 }),
-                fill: false,
-                borderColor: 'rgba(255, 99, 132, 1)', // Cor da linha
-                tension: 0.1,
-                pointBackgroundColor: 'rgba(255, 99, 132, 1)', // Cor dos pontos
+                borderColor: 'rgba(255, 99, 132, 1)',
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                pointBackgroundColor: 'rgba(255, 99, 132, 1)',
                 pointBorderColor: '#fff',
-                pointRadius: 5,
-                pointHoverRadius: 7,
+                tension: 0.1,
             },
         ],
     };
 
-    // Log final para verificar todos os dados de tempos de requisição processados
-    console.log('Dados do gráfico:', data.datasets[0].data);
+    const chartOptions = {
+        responsive: true,
+        scales: {
+            x: {
+                ticks: { color: '#a9a9a9' },
+                grid: { color: '#333333' },
+            },
+            y: {
+                beginAtZero: true,
+                ticks: { color: '#a9a9a9' },
+                grid: { color: '#333333' },
+                title: { display: true, text: 'Tempo de Resposta (ms)', color: '#ffffff' },
+            },
+        },
+        plugins: {
+            legend: { labels: { color: '#ffffff' } },
+            tooltip: {
+                callbacks: {
+                    label: (tooltipItem) => `Tempo: ${tooltipItem.raw} ms` || 'Erro',
+                },
+            },
+        },
+    };
 
     return (
         <ThemeProvider theme={darkTheme}>
@@ -113,80 +152,49 @@ function ApiDetails({ apiUrl, apiName }) {
                 <Typography variant="h4" gutterBottom>
                     Logs da API: {apiName}
                 </Typography>
-                <Select
-                    value={timeFilter}
-                    onChange={(e) => setTimeFilter(e.target.value)}
-                    displayEmpty
-                    inputProps={{ 'aria-label': 'Filtro de Tempo' }}
-                    style={{ marginBottom: '20px', color: '#ffffff' }}
-                >
-                    <MenuItem value="last12h">Últimas 12 horas</MenuItem>
-                    <MenuItem value="last24h">Últimas 24 horas</MenuItem>
-                    <MenuItem value="last7d">Últimos 7 dias</MenuItem>
-                    <MenuItem value="last30d">Últimos 30 dias</MenuItem>
-                </Select>
+                <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+                    <Select
+                        value={timeFilter}
+                        onChange={(e) => setTimeFilter(e.target.value)}
+                        displayEmpty
+                        inputProps={{ 'aria-label': 'Filtro de Tempo' }}
+                        style={{ color: '#ffffff' }}
+                    >
+                        <MenuItem value="last12h">Últimas 12 horas</MenuItem>
+                        <MenuItem value="last24h">Últimas 24 horas</MenuItem>
+                        <MenuItem value="last7d">Últimos 7 dias</MenuItem>
+                        <MenuItem value="last30d">Últimos 30 dias</MenuItem>
+                    </Select>
+                    <Select
+                        value={selectedDatabase}
+                        onChange={(e) => setSelectedDatabase(e.target.value)}
+                        displayEmpty
+                        inputProps={{ 'aria-label': 'Selecionar Banco' }}
+                        style={{ color: '#ffffff' }}
+                    >
+                        <MenuItem value="Database1">Database 1</MenuItem>
+                        <MenuItem value="Database2">Database 2</MenuItem>
+                        <MenuItem value="Database3">Database 3</MenuItem>
+                    </Select>
+                </div>
+                <Button variant="contained" color="primary" onClick={toggleChartType}>
+                Tipo do gráfico
+                </Button>
+
                 {loading ? (
                     <CircularProgress color="inherit" />
                 ) : error ? (
-                    <Typography variant="body1" color="error">{`Erro ao buscar dados: ${error}`}</Typography>
+                    <Typography variant="body1" color="error">
+                        {`Erro ao buscar dados: ${error}`}
+                    </Typography>
                 ) : filteredLogs.length === 0 ? (
-                    <Typography variant="body1" color="secondary">Nenhum dado disponível para o período selecionado. Verifique se a URL da API está correta e se existem dados para o endpoint.</Typography>
+                    <Typography variant="body1" color="secondary">
+                        Nenhum dado disponível para o período selecionado.
+                    </Typography>
+                ) : chartType === 'line' ? (
+                    <Line data={data} options={chartOptions} />
                 ) : (
-                    <Line
-                        data={data}
-                        options={{
-                            responsive: true,  // Garante que o gráfico seja responsivo
-                            scales: {
-                                x: {
-                                    ticks: {
-                                        color: '#a9a9a9',
-                                    },
-                                    grid: {
-                                        color: '#333333',
-                                    },
-                                },
-                                y: {
-                                    beginAtZero: true, // Garante que o eixo Y comece do zero
-                                    ticks: {
-                                        stepSize: 1, // Define o intervalo entre as marcações
-                                        color: '#a9a9a9',
-                                    },
-                                    grid: {
-                                        color: '#333333',
-                                    },
-                                    title: {
-                                        display: true,
-                                        text: 'Tempo de Resposta (ms)',
-                                        color: '#ffffff',
-                                    },
-                                },
-                            },
-                            plugins: {
-                                legend: {
-                                    labels: {
-                                        color: '#ffffff',
-                                    },
-                                },
-                                tooltip: {
-                                    enabled: true,
-                                    mode: 'index',
-                                    intersect: false,
-                                    callbacks: {
-                                        title: (tooltipItems) => {
-                                            const date = new Date(tooltipItems[0].label);
-                                            return `${date.getDate().toString().padStart(2, '0')}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
-                                        },
-                                        label: (tooltipItem) => {
-                                            return tooltipItem.raw !== null ? `Tempo: ${tooltipItem.raw} ms` : 'Erro';
-                                        },
-                                    },
-                                    backgroundColor: '#333333',
-                                    titleColor: '#ffffff',
-                                    bodyColor: '#ffffff',
-                                },
-                            },
-                        }}
-                    />
+                    <Bar data={data} options={chartOptions} />
                 )}
             </Container>
         </ThemeProvider>
